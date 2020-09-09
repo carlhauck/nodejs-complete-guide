0940542821745ef395afbed308394c4c4fcbd925
carlhauck <carlhauck@gmail.com>
2020-09-09 16:00:28 -0500

    installs express.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next(); // Allows the request to continue to the next middleware line
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
