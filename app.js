409a0b81663e9c17c67a7005ce1b40a151294a59
carlhauck <carlhauck@gmail.com>
2020-09-18 13:23:23 -0500

    adds and serves html pages using path module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,6 +6,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { dirname } = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
