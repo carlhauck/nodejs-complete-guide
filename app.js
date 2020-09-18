e989e881bfdc09b5e4fb4f138dc21077f4135570
carlhauck <carlhauck@gmail.com>
2020-09-18 12:31:32 -0500

    splits routes out into separate admin and shop js files

--- app.js
+++ app.js
@@ -3,26 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button tpe="submit">Add Product</button></form>');
-});
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);
